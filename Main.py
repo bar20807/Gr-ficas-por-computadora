2c76009c376867e153ca4911910a5dfd16c79671
José Rodrigo Barrera <bar20807@uvg.edu.gt>
2022-11-16 19:55:27 -0600

    Proyecto#1 (COMPLETADO)

--- Main.py
+++ Main.py
@@ -19,10 +19,10 @@ def SR1(x,y,r,g,b):
     MyRender.glFinish('b.bmp')
 
 def SR2(x0,y0,x1,y1):
-    
+
     V0=V3(x0,y0)
     V1=V3(x1,y1)
-    
+
     #INSTANCIAS DE MI RENDER LÍNEA
     MyRender = Render(500,500)
     MyRender.glViewport(0, 0, 500, 500)
@@ -33,29 +33,29 @@ def SR2(x0,y0,x1,y1):
     MyRender.glFinish('line.bmp')
 
 def DrawHouse():
-     
+
     #VECTORES PARA DIBUJAR LA CASA
     #V0=V(0.3,-0.5)
     #V1=V(0.3,-0.1)
-    
+
     #INSTANCIAS
     MyRender = Render(500,500)
     MyRender.glViewport(0,0,500,500)
     MyRender.glClearColor(0.5,0.5,0.5)
     MyRender.glClear()
     MyRender.glClearViewport(color(0,0,0))
-    
+
     #PRIMERA PARED
     MyRender.glLine(V3(0.2,-0.4),V3(0.2,-0.05),color(1,0,0)) #LÍNEA 1 DEL CUADRADO
     MyRender.glLine(V3(0.6,-0.4),V3(0.6,-0.05),color(1,0,0)) #LÍNEA 2 BAJA DEL CUADRADO
     MyRender.glLine(V3(0.2,-0.4),V3(0.6,-0.4),color(1,0,0))  #LÍNEA 3 IZQUIERDA DEL CUADRADO
     MyRender.glLine(V3(0.2,-0.05),V3(0.6,-0.05),color(1,0,0)) #LÍNEA 4 DERECHA DEL CUADRADO
-    
+
     #TECHO DE LA PARED
     MyRender.glLine(V3(0.6,-0.4),V3(0.8,-0.2),color(1,1,0))
     MyRender.glLine(V3(0.6,-0.05),V3(0.8,-0.2),color(0,1,0))
-    
-    #SEGUNDA PARED (FRONTAL)    
+
+    #SEGUNDA PARED (FRONTAL)
     MyRender.glFinish('house.bmp')
 
 def DrawPolligons():
@@ -64,7 +64,7 @@ def DrawPolligons():
     MyRender.glClearColor(0.5,0.5,0.5)
     MyRender.glClear()
     MyRender.glClearViewport(color(0,0,0))
-    
+
     star = [(165, 380), (185, 360), (180, 330), (207, 345), (233, 330),
         (230, 360), (250, 380), (220, 385), (205, 410), (193, 383)]
 
@@ -90,9 +90,9 @@ def DrawPolligons():
     MyRender.scanFillpoly(pol4, color(1,1,1))
     MyRender.drawPolygon(pol5, color(1,0,0))
     MyRender.scanFillpoly(pol5, color(1,0,0))
-    
+
     MyRender.glFinish("Poligonos.bmp")
-    
+
 
 def Read_Objects():
     MyRender = Render(2024, 2024)
@@ -100,37 +100,53 @@ def Read_Objects():
     MyRender.lookAt(V3(0,0,1), V3(0,0,0),V3(0,1,0))
     MyRender.display_obj('Mask.obj', translate=(0, 0, 0), scale=(5, 5, 5),rotate=(0,0,pi/6), texture=t)
     MyRender.glFinish('DutchAngle(DA).bmp')
-    
+
 def Planet():
     MyRender = Render(600, 600)
     MyRender.load_shaders("sphere.obj", tras=(370, 395, 0), size=(280, 280, 280), shape='Jupiter')
     MyRender.glFinish(filename="Jupiter.bmp")
 
-def Proyecto1():
-    MyRender = Render(1024, 1024)
-    MyRender.glClearViewport(color(0,0,0))
-    t1=Texture('./Proyecto1/Mask1Texture.bmp')
+def escena_project():
+    MyRender = Render(2024, 2024)
     MyRender.lookAt(V3(0,0,1), V3(0,0,0),V3(0,1,0))
-    MyRender.display_obj('./Proyecto1/Mask1.obj', translate=(0, 0.6, 0), scale=(3, 3, 5),rotate=(0,0,0), texture=t1)
-    t2=Texture('./Proyecto1/Mask2Texture.bmp')
-    MyRender.display_obj('./Proyecto1/Mask2.obj', translate=(-0.6, 0.4, 0), scale=(3, 3, 5),rotate=(0,0,0), texture=t2)
-    t3=Texture('./Proyecto1/Mask2Texture.bmp')
-    MyRender.display_obj('./Proyecto1/Mask2.obj', translate=(0.6, 0.4, 0), scale=(3, 3, 5),rotate=(0,0,0), texture=t3)
-    t4=Texture('./Proyecto1/VF_Coffee_Table_Textures_BaseColor.bmp')
-    MyRender.display_obj('./Proyecto1/Vintage_Furniture_Coffee_Table_OBJ.obj', translate=(0, 0, 0), scale=(100, 100, 120),rotate=(0,0,0), texture=t4)
-    print("Posición final")
+    
+    #Cargamos el fondo
+    tbg = Texture('./Proyecto1/texturebg.bmp')
+    MyRender.framebuffer = tbg.pixels
+    
+    #Cargamos las máscaras
+    t1=Texture('./Proyecto1/Mask2Texture.bmp')
+    MyRender.display_obj('./Proyecto1/Mask2.obj', translate=(0, 0.6, 0), scale=(3, 3, 5),rotate=(0,0,0), texture=t1)
+    
+    t2=Texture('./Proyecto1/Mask1Texture.bmp')
+    MyRender.display_obj('./Proyecto1/Mask1.obj', translate=(-0.6, 0.4, 0), scale=(3, 3, 5),rotate=(0,0,0), texture=t2)
+    
+    t3=Texture('./Proyecto1/Mask3Texture.bmp')
+    MyRender.display_obj('./Proyecto1/Mask3.obj', translate=(0.5, 0.07, 0), scale=(1.5, 1.5, 2.5),rotate=(0,pi/9,0), texture=t3)
+    
+    #Cargamos el mueble
+    t4=Texture('./Proyecto1/old_rustic_stand_Mat_BaseColor.bmp')
+    MyRender.display_obj('./Proyecto1/old_rustic_stand.obj', translate=(0, -0.88, 0), scale=(0.7, 0.7, 0.7),rotate=(0,pi,0), texture=t4)
+    
+    # #Cargamos la planta
+    t5=Texture('./Proyecto1/b3.bmp')
+    MyRender.display_obj('./Proyecto1/Pot.obj', translate=(0, -0.2, 0), scale=(0.1, 0.1, 0.1),rotate=(0,0,0), texture=t5)
+    
+    #Testeamos la llegada a la escritura
+    print("Modelos cargados")
     MyRender.glFinish('Proyecto1.bmp')
+    
 
 iterador= False
 opciones= 0
 
 while not iterador:
-    
+
     print("-+-+-+-+-+-+-+-+-+-+-+-+-+ BIENVENIDO A MI RENDER -+-+-+-+-+-+-+-+-+-+-+-+-+")
     print("\n 1) Mostrar SR1 \n 2) Mostrar SR2" "\n 3) Dibujar una casa utilizando glLine \n 4) Dibujar poligonos \n 5) Renderizar un modelo \n 6) Planeta con Shaders \n 7) Proyecto \n 8) Salir ")
     opciones= int(input("\n Elige una de las siguientes opciones: "))
-    
-    
+
+
     try:
         if opciones == 1:
             x = float(input("Porfavor, ingrese una coordenada en X con rango de (0 a 1): "))
@@ -138,37 +154,37 @@ while not iterador:
             r= float(input("Porfavor, ingrese el color r con rango de (0 a 1): "))
             g= float(input("Porfavor, ingrese el color g con rango de (0 a 1): "))
             b= float(input("Porfavor, ingrese el color b con rango de (0 a 1): "))
-            
+
             if 0<r<1 & 0<g<1 & 0<b<1:
                 print("Usted no ingresó un valor válido en rango indicado")
             else:
                 SR1(x,y,r,g,b)
-                
+
         elif opciones == 2:
-            
+
             x0=float(input("Porfavor, ingrese su coordenada inicial (x0): "))
             y0=float(input("Porfavor, ingrese su coordenada inicial (y0): "))
             x1=float(input("Porfavor, ingrese su coordenada inicial (x1): "))
             y1=float(input("Porfavor, ingrese su coordenada inicial (y1): "))
-            
+
             SR2(x0,y0,x1,y1)
-            
+
         elif opciones == 3:
             DrawHouse()
-        
+
         elif opciones==4:
             DrawPolligons()
-        
+
         elif opciones==5:
             Read_Objects()
         elif opciones==6:
             Planet()
         elif opciones==7:
-            Proyecto1()
+            escena_project()
         elif opciones==8:
             iterador= True
             print("Gracias por usar mi render")
-            
+
     except Exception as e:
         print( e + "\nUsted ingresó un carecter no válido... Intente de nuevo")
 
