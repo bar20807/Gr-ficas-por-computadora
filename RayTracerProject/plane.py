49f3984b11f163e749b240bd2d7d0c6600cd4acb
José Rodrigo Barrera <bar20807@uvg.edu.gt>
2022-11-19 23:04:14 -0600

    Arreglando errores del plano

--- RayTracerProject/plane.py
+++ RayTracerProject/plane.py
@@ -7,18 +7,24 @@ from material import *
 from color import *
 
 class Plane(object):
-    def __init__(self, position, normal, material):
-        self.position = position
-        self.normal =  normal.norm()
+    def __init__(self, center, width, large, material):
+        self.center = center
+        self.width = width
+        self.large = large
         self.material = material
-
-    def ray_intersect(self, orig, dir):
-        denom = dir @ self.normal
-        if abs(denom) > 0.0001:
-            t = (self.normal @ (self.position - orig)) / denom
-            if t > 0:
-                hit = orig + (dir*t)
-                return Intersect(distance = t,
-                                 point = hit,
-                                 normal = self.normal)
-        return None
+        
+    def ray_intersect(self, origin, direction):
+        d = (origin.y + self.center.y) / direction.y
+        impact = origin + (direction * d)
+        normal = V3(0, 1, 0)
+        
+        if (d <= 0) or \
+            impact.x > (self.center.x + self.width/2) or impact.x < (self.center.x - self.width/2) or \
+            impact.z > (self.center.z + self.large/2) or impact.z < (self.center.z - self.large/2): 
+            return None
+        
+        return Intersect(
+            distance=d,
+            point=impact,
+            normal=normal
+        )
